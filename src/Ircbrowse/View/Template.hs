120fb95ae7272c335eb2c6d4477dfe9cabeea18b
Tom Smeding <tom.smeding@gmail.com>
2020-09-16 20:49:12 +0200

    Remove google analytics

--- src/Ircbrowse/View/Template.hs
+++ src/Ircbrowse/View/Template.hs
@@ -19,17 +19,7 @@ template name thetitle innerhead innerbody = do
               link ! rel "stylesheet" ! type_ "text/css" ! href "/css/ircbrowse.css"
               meta ! httpEquiv "Content-Type" ! content "text/html; charset=UTF-8"
               innerhead
-    body !# name $ do
-      innerbody
-      preEscapedToHtml ("<script type=\"text/javascript\"> var _gaq = _gaq \
-                       \|| []; _gaq.push(['_setAccount', 'UA-38975161-1']);\
-                       \ _gaq.push(['_trackPageview']); (function() {var ga\
-                       \ = document.createElement('script'); ga.type = 'tex\
-                       \t/javascript'; ga.async = true; ga.src = ('https:' \
-                       \== document.location.protocol ? 'https://ssl' : \
-                       \'http://www') + '.google-analytics.com/ga.js'; var\
-                       \ s = document.getElementsByTagName('script')[0]; \
-                       \s.parentNode.insertBefore(ga, s);})(); </script>" :: Text)
+    body !# name $ innerbody
 
 channelNav :: Channel -> Html
 channelNav channel =
